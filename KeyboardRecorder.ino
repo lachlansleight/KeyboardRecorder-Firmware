ee647686db166bf28462a7d455308147f5cbf3f1
Lachlan Sleight <lachlansleight@gmail.com>
2021-03-23 12:23:22 +1100

    feat: Basic ESP32 MIDI Input working
    + Holy fucking god that was annoying

--- KeyboardRecorder.ino
+++ KeyboardRecorder.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <MIDI.h>
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial2, MIDI);
 
+void setup() {
+  MIDI.setHandleNoteOn(handleNoteOn);
+  MIDI.setHandleNoteOff(handleNoteOff);
+  MIDI.setHandleControlChange(handleControlChange);
+  Serial.begin(38400);
+
+  Serial.println("MIDI TEST BEGIN");
+
+  MIDI.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  MIDI.read();
+}
 
+void handleNoteOn(byte channel, byte pitch, byte velocity)
+{
+    Serial.println("ON\t" + String(channel) + "\t" + String(pitch) + "\t" + String(velocity));
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity)
+{
+    Serial.println("OFF\t" + String(channel) + "\t" + String(pitch) + "\t" + String(velocity));
+}
+
+void handleControlChange(byte channel, byte number, byte value)
+{
+    Serial.println("CC\t" + String(channel) + "\t" + String(number) + "\t" + String(value));
 }
